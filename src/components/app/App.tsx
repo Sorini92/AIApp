6366adca528fece34ffcaa484b813529c09eb1ba
Rus <bedlinskiyruslan@gmail.com>
2024-03-21 09:54:59 +0200

    changes for merge

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,8 +2,8 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "../../theme";
-import { Auth } from "../../pages/auth/Auth";
-import { AIChat } from "../../pages/aiChat/AIChat";
+import { Auth } from "../../pages/Auth/Auth";
+import { AIChat } from "../../pages/AIChat/AIChat";
 import { Navigation } from "../../components/navigation";
 import { Box } from "@mui/material";
 import { Community } from "../../pages/community/Community";
@@ -28,60 +28,60 @@ import { Settings } from "../../pages/settings/Settings";
 } */
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    //errorElement: <PageNotFound />,
-    children: [
-      {
-        path: "/",
-        element: <AIChat />,
-      },
-      {
-        path: "/aichat",
-        element: <AIChat />,
-      },
-      {
-        path: "/community",
-        element: <Community />,
-      },
-      {
-        path: "/inbox",
-        element: <Inbox />,
-      },
-      {
-        path: "/settings",
-        element: <Settings />,
-      },
-      {
-        path: "/auth/signup",
-        element: <Auth component="signup" />,
-      },
-      {
-        path: "/auth/login",
-        element: <Auth component="login" />,
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: <Root />,
+		//errorElement: <PageNotFound />,
+		children: [
+			{
+				path: "/",
+				element: <AIChat />,
+			},
+			{
+				path: "/aichat",
+				element: <AIChat />,
+			},
+			{
+				path: "/community",
+				element: <Community />,
+			},
+			{
+				path: "/inbox",
+				element: <Inbox />,
+			},
+			{
+				path: "/settings",
+				element: <Settings />,
+			},
+			{
+				path: "/auth/signup",
+				element: <Auth component="signup" />,
+			},
+			{
+				path: "/auth/login",
+				element: <Auth component="login" />,
+			},
+		],
+	},
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
 
 function Root() {
-  return (
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <ThemeProvider theme={theme}>
-        <Box sx={{ display: "flex" }}>
-          <Navigation />
-          <Box sx={{ padding: "0 24px", height: "100vh", width: "100%" }}>
-            <Outlet />
-          </Box>
-        </Box>
-      </ThemeProvider>
-    </GoogleOAuthProvider>
-  );
+	return (
+		<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+			<ThemeProvider theme={theme}>
+				<Box sx={{ display: "flex" }}>
+					<Navigation />
+					<Box sx={{ padding: "0 24px", height: "100vh", width: "100%" }}>
+						<Outlet />
+					</Box>
+				</Box>
+			</ThemeProvider>
+		</GoogleOAuthProvider>
+	);
 }
 
 export default App;
